8b61376e1f2a1d902e5076fabc1ef8a853c62d3c
John Rowley <outsidethebox@eircom.net>
2020-09-23 08:59:36 +0100

    First Commit Week 1

---
+++ app/src/main/java/com/example/android/aboutme/MyName.kt
@@ -0,0 +1,9 @@
+
+package com.example.android.aboutme
+
+// Create data class MyName for the name and nickname here.
+
+data class MyName(var name: String = "", var nickname: String = "")
+
+data class MyAddress(var address1: String = "", var country:String = "")
+
